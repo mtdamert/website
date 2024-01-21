65e93f6ed5cae5def69ecbe17ee64839affbfc54
Mike Damert <mtdamert@gmail.com>
2024-01-21 14:20:56 -0500

    Changed page title

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import useNavigation from '../hooks/use-navigation';
 
 function Route({ path, children }) {
+    useEffect(() => {
+        document.title = 'mtdamert.com';
+      }, []);
+
     const { currentPath } = useNavigation();
 
     if (path === currentPath) {
