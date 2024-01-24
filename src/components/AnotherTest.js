43d4b889267820474e43ae2fa03bb9d465c7ccf8
Mike Damert <mtdamert@gmail.com>
2024-01-24 13:28:28 -0500

    Added tests of React functions

--- src/components/AnotherTest.js
+++ src/components/AnotherTest.js
@@ -1,6 +1,17 @@
 function AnotherTest() {
+    const firstFunc = () => {
+        return 'hello ';
+    }
+
+    const secondFunc = () => {
+        return firstFunc() + 'world';
+    }
+
     return (
-        <div className="flex items-center px-3 py-1.5 border">Second test</div>
+        <div className="flex items-center px-3 py-1.5 border">
+            Second test<br />
+            {secondFunc()}
+        </div>
     );
 }
 
