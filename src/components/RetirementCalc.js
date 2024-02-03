5ac55f76b7e2dbfa21381dec3f319d24564e053f
Mike Damert <mtdamert@gmail.com>
2024-02-03 10:28:06 -0500

    Added formatting; added income as an input

--- src/components/RetirementCalc.js
+++ src/components/RetirementCalc.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 
 function RetirementCalc() {
     const [savings, setSavings] = useState(0);
-    const [livingExpenses, setLivingExpenses] = useState(30000);
+    const [income, setIncome] = useState(0);
+    const [livingExpenses, setLivingExpenses] = useState(35000);
     const [currentAge, setCurrentAge] = useState(40);
     const [retirementAge, setRetirementAge] = useState(65);
     const [predictedYield, setPredictedYield] = useState(8);
@@ -19,17 +20,18 @@ function RetirementCalc() {
         while (remainingSavings > 0 && ageCounter < 200) {
             remainingSavings -= yearlyExpenses;
             yearlyExpenses *= 1.03;
-            remainingSavings *= 1.0 + (predictedYield * 0.01);
+            remainingSavings *= (1.0 + (predictedYield * 0.01));
+            if (currentAge < retirementAge)
+                remainingSavings += Number(income);
 
             console.log("age: " + ageCounter);
             console.log("savings: " + remainingSavings);
             console.log("expenses: " + yearlyExpenses);
 
-
             ageCounter += 1;
         }
 
-        if (ageCounter == currentAge)
+        if (ageCounter === currentAge)
             return currentAge;
         else
             return ageCounter - 1;
@@ -38,64 +40,80 @@ function RetirementCalc() {
     return (
         <div className="flex items-center px-3 py-1.5 border">
             <form onSubmit={handleSubmit}>
-            <div>
-                <label>
+            <div className="pt-2">
+                <label className="font-semibold">
                     Current Age: 
+                    </label>
                     <input type="number"
                         value={currentAge}
                         onChange={(e) => setCurrentAge(e.target.value)}
+                        className="float-right border"
                     />
-                </label>
             </div>
 
-            <div>
-                <label>
+            <div className="pt-2">
+                <label className="font-semibold">
                     Current Savings:
-                    <input type="text"
-                        value={savings}
-                        onChange={(e) => setSavings(e.target.value)} 
-                    />
                 </label>{/* todo: sanitize input */}
+                <input type="text"
+                    value={savings}
+                    onChange={(e) => setSavings(e.target.value)} 
+                    className="float-right border"
+                />
             </div>
 
-            <div>
-                <label>
-                    Living Expenses:
-                    <input type="text"
-                        value={livingExpenses}
-                        onChange={(e) => setLivingExpenses(e.target.value)}
-                    />
-                </label>{/* todo: sanitize input */}
-            </div>
-
-            <div>
-                <label>
-                    Retirement Age:
-                    <input type="number"
-                        value={retirementAge}
-                        onChange={(e) => setRetirementAge(e.target.value)}
-                    />
+            <div className="pt-2">
+                <label className="font-semibold">
+                    Current Income:
                 </label>
+                <input type="text"
+                    value={income}
+                    onChange={(e) => setIncome(e.target.value)}
+                    className="float-right border"
+                />
+            </div>
+
+            <div className="pt-2">
+                <label className="font-semibold">
+                    Living Expenses:
+                </label>{/* todo: sanitize input */}
+                <input type="text"
+                    value={livingExpenses}
+                    onChange={(e) => setLivingExpenses(e.target.value)}
+                    className="float-right border"
+                />
+            </div>
+
+            <div className="pt-2">
+                <label className="font-semibold">
+                    Retirement Age:
+                </label>
+                <input type="number"
+                    value={retirementAge}
+                    onChange={(e) => setRetirementAge(e.target.value)}
+                    className="float-right border"
+                />
             </div>{/* todo: Use this value, and also add a 'yearly income' value */}
 
-            <div>
-                <label>
+            <div className="pt-2">
+                <label className="font-semibold">
                     Predicted Yield:
-                    <input type="number"
-                        value={predictedYield}
-                        onChange={(e) => setPredictedYield(e.target.value)}
-                    /> %
                 </label>
+                <span className="float-right">%</span>
+                <input type="number"
+                    value={predictedYield}
+                    onChange={(e) => setPredictedYield(e.target.value)}
+                    className="float-right border"
+                />
             </div>
 
-            <div>Assuming 3% inflation</div>{/* todo: make this a variable */}
+            <div className="italic pt-2 pl-4">Assuming 3% inflation</div>{/* todo: make this a variable */}
 
-            <div>
-                <input type="submit" />
-            </div>
+            <div className="pt-6"><span className="font-semibold">At the current rate, your savings will run out when you reach age:</span> {getSavingsZeroAge()}</div>
+
             </form>
 
-            <div>At the current rate, your savings will run out when you reach age: {getSavingsZeroAge()}</div>
+
         </div>
     );
 }
