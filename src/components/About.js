8a8b302b9abfce05e47f08543f2733eea8c722bd
Mike Damert <mtdamert@gmail.com>
2024-02-04 16:11:21 -0500

    Updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 function About() {
     return (
-        <div className="flex items-center px-3 py-1.5 border">Hi, I'm mtdamert.</div>
+        <div className="flex items-center px-3 py-1.5 border">Hi, I'm Mike. </div>
     );
 }
 
