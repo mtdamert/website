99831a725ac9e641cddceee7b34dce4545316119
Mike Damert <mtdamert@gmail.com>
2024-02-07 17:17:44 -0500

    Started capital gains calculations

--- src/components/RetirementCalc.js
+++ src/components/RetirementCalc.js
@@ -9,6 +9,7 @@ function RetirementCalc() {
     const [startSocialSecurityAge, setStartSocialSecurityAge] = useState(0);
     const [socialSecurityIncome, setSocialSecurityIncome] = useState(0);
     const [predictedYield, setPredictedYield] = useState(8);
+    const [extrapolateCapGains, setExtrapolateCapGains] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -20,8 +21,12 @@ function RetirementCalc() {
         let yearlyExpenses = livingExpenses;
         let sSIncome = socialSecurityIncome;
 
-        {/* TODO: Cap gains tax */}
-        {/* TODO: Break savings into basic and retirement (and add fee for withdrawal before age 59.5) */}
+        let capGainsLow = 44625;
+        let capGainsHigh = 492300;
+
+        // TODO: Cap gains tax - this can also be married (joint filing), married (separate filing), or head of household
+        // TODO: Add cost basis
+        // TODO: Break savings into basic and retirement (and add fee for withdrawal before age 59.5)
 
         while (remainingSavings > 0 && ageCounter < 200) {
             remainingSavings -= yearlyExpenses;
@@ -32,6 +37,11 @@ function RetirementCalc() {
                 remainingSavings += Number(income);
             if (currentAge >= startSocialSecurityAge)
                 remainingSavings += sSIncome;
+
+            if (extrapolateCapGains === true) {
+                capGainsLow *= 1.03;
+                capGainsHigh *= 1.03;
+            }
 
             console.log("age: " + ageCounter);
             console.log("savings: " + remainingSavings);
@@ -135,6 +145,17 @@ function RetirementCalc() {
                 <input type="number"
                     value={predictedYield}
                     onChange={(e) => setPredictedYield(e.target.value)}
+                    className="float-right border"
+                />
+            </div>
+
+            <div className="pt-2">
+                <label className="font-semibold">
+                    Extrapolate Capital Gains based on 2024 Rate
+                </label>
+                <input type="checkbox"
+                    value={extrapolateCapGains}
+                    onChange={(e) => setExtrapolateCapGains(e.target.value)}
                     className="float-right border"
                 />
             </div>
