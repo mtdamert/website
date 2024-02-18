028d70b319c280573a19c13c5c1e6a9c7dbb39c1
Mike Damert <mtdamert@gmail.com>
2024-02-18 11:30:05 -0500

    Added basic tooltips on mouseover

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,6 +1,8 @@
+import Tooltip from './Tooltip';
+
 function Test() {
     return (
-        <div className="flex items-center px-3 py-1.5 border">Test test test</div>
+        <div className="flex items-center px-3 py-1.5 border">Test test test<Tooltip text="test tooltip" /></div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
       extend: {},
     },
     plugins: [],
+    variants: {
+      extend: {
+          display: ["group-hover"],
+      },
+  },
   }
   
   
