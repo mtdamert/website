ff60e41971647fb5c83469c2d678e24a315b5a51
Mike Damert <mtdamert@gmail.com>
2024-03-06 09:53:00 -0500

    Updated high scores

--- asp_website.Server/Controllers/TetrisHighScoresController.cs
+++ asp_website.Server/Controllers/TetrisHighScoresController.cs
@@ -17,17 +17,18 @@ namespace asp_website.Server.Controllers
             TetrisHighScores[] tetrisHighScores = new TetrisHighScores[5];
             for (int i=0; i<5; i++) { tetrisHighScores[i] = new TetrisHighScores(); }
 
-            tetrisHighScores[0].HighScore = 29740;
-            tetrisHighScores[0].ScorerName = "Testing";
+            tetrisHighScores[0].HighScore = 124380;
+            tetrisHighScores[0].ScorerName = "mtdamert";
             tetrisHighScores[0].IsCurrentScore = false;
 
-            tetrisHighScores[1].HighScore = 25640;
-            tetrisHighScores[1].ScorerName = "Still Testing";
+            tetrisHighScores[1].HighScore = 29740;
+            tetrisHighScores[1].ScorerName = "Testing";
             tetrisHighScores[1].IsCurrentScore = false;
 
-            tetrisHighScores[2].HighScore = 3000;
-            tetrisHighScores[2].ScorerName = "More Testing";
+            tetrisHighScores[2].HighScore = 25640;
+            tetrisHighScores[2].ScorerName = "Still Testing";
             tetrisHighScores[2].IsCurrentScore = false;
+
             tetrisHighScores[3].HighScore = 2000;
             tetrisHighScores[3].ScorerName = "Testing Again";
             tetrisHighScores[3].IsCurrentScore = false;
