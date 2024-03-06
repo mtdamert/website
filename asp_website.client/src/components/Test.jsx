e00295418c88d83aaaf8786ae24d07d583f5cf8b
Mike Damert <mtdamert@gmail.com>
2024-03-06 10:07:18 -0500

    Rewrote tooltips

--- asp_website.client/src/components/Test.jsx
+++ asp_website.client/src/components/Test.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Tooltip from './Tooltip';
+import Popup from 'reactjs-popup';
 
 function Test() {
     const [testData, setTestData] = useState();
@@ -9,12 +10,13 @@ function Test() {
     }, []);
 
     const testDataContent = testData === undefined
-        ? <p><em>Loading test data... Please refresh once the ASP.NET backend has started.</em></p>
+        ? <p><em>Loading test data...</em></p>
         : <div>{testData}</div>
 
     async function populateTestData() {
         const response = await fetch('testdata');
         const data = await response.text();
+
         setTestData(data);
     }
 
@@ -29,10 +31,25 @@ function Test() {
     return (
         <div className="flex items-center px-3 py-1.5 border">
             <div>
-                Test test test<Tooltip text="test tooltip" />
+                Test test test
             </div><br />
             <div>{secondFunc()}</div><br />
             <div className="pt-2">{testDataContent}</div>
+            <div>
+                <Popup
+                    trigger={<div className="menu-item"> ? </div>}
+                    position="right top"
+                    on="hover"
+                    closeOnDocumentClick
+                    mouseLeaveDelay={300}
+                    mouseEnterDelay={0}
+                    contentStyle={{ padding: '0px', border: 'none' }}
+                    arrow={false}
+                >
+                    <span> Test tooltip </span>
+                </Popup>
+
+            </div>
         </div>
     );
 }
