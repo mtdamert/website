621b832a9fae6f56d1890afe259f5956d5248493
Mike Damert <mtdamert@gmail.com>
2024-03-14 10:00:26 -0400

    If high scores don't load, set them all to zero

--- asp_website.Server/Controllers/TetrisHighScoresController.cs
+++ asp_website.Server/Controllers/TetrisHighScoresController.cs
@@ -8,6 +8,8 @@ namespace asp_website.Server.Controllers
     [Route("[controller]")]
     public class TetrisHighScoresController : ControllerBase
     {
+        const int NUM_HIGH_SCORES = 5;
+
         int revisionNumber;
         TetrisHighScores[]? highScores;
         const string highScoresPath = "TetrisHighScores.txt";
@@ -24,35 +26,14 @@ namespace asp_website.Server.Controllers
         [HttpGet(Name = "GetTetrisHighScores")]
         public TetrisHighScores[] Get()
         {
-            //TetrisHighScores[] tetrisHighScores = new TetrisHighScores[5];
-            //for (int i=0; i<5; i++) { tetrisHighScores[i] = new TetrisHighScores(); }
-
-            //tetrisHighScores[0].HighScore = 124380;
-            //tetrisHighScores[0].ScorerName = "mtdamert";
-            //tetrisHighScores[0].IsCurrentScore = false;
-
-            //tetrisHighScores[1].HighScore = 29740;
-            //tetrisHighScores[1].ScorerName = "Testing";
-            //tetrisHighScores[1].IsCurrentScore = false;
-
-            //tetrisHighScores[2].HighScore = 25640;
-            //tetrisHighScores[2].ScorerName = "Still Testing";
-            //tetrisHighScores[2].IsCurrentScore = false;
-
-            //tetrisHighScores[3].HighScore = 2000;
-            //tetrisHighScores[3].ScorerName = "Testing Again";
-            //tetrisHighScores[3].IsCurrentScore = false;
-            //tetrisHighScores[4].HighScore = 1000;
-            //tetrisHighScores[4].ScorerName = "Final Testing";
-            //tetrisHighScores[4].IsCurrentScore = false;
-
-            //string jsonString = JsonSerializer.Serialize(tetrisHighScores);
-            //using (StreamWriter writer = new StreamWriter(highScoresPath))
-            //{
-            //    writer.WriteLine(jsonString);
-            //}
-
-            //return tetrisHighScores;
+            if (highScores == null) {
+                highScores = new TetrisHighScores[NUM_HIGH_SCORES];
+                for (int i=0; i< NUM_HIGH_SCORES; i++) {
+                    highScores[i].HighScore = 0;
+                    highScores[i].ScorerName = "";
+                    highScores[i].IsCurrentScore = false;
+                }
+            }
 
             return highScores;
         }
