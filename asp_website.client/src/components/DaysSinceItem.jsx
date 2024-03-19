2f9280b38533c8b29ac2000e32fc9ed3ee2685a8
Mike Damert <mtdamert@gmail.com>
2024-03-19 18:03:40 -0400

    Initial add of Days Since Item

---
+++ asp_website.client/src/components/DaysSinceItem.jsx
@@ -0,0 +1,15 @@
+
+// TODO: Last Date
+// TODO: Color (green/red)
+// TODO: Name
+
+function DaysSinceItem({ name }) {
+    return (
+        <div
+            className="bg-rose-700 text-white w-4/5 h-1/6"> // bg-emerald-700
+            {name}
+        </div>
+    );
+}
+
+export default DaysSinceItem;
