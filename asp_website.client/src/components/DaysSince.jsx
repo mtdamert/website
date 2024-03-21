4fc34d900b9feeab2e40aa861ff0587e891d0c4d
Mike Damert <mtdamert@gmail.com>
2024-03-21 10:39:26 -0400

    Create new items in the days since app when the new item button is clicked

--- asp_website.client/src/components/DaysSince.jsx
+++ asp_website.client/src/components/DaysSince.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import DaysSinceItem from "./DaysSinceItem";
 
 function DaysSince() {
+    const [items, setItems] = useState([]);
+
     const addItem = () => {
-        // TODO
+        setItems([
+            ...items,
+            new DaysSinceItem("New Item", false)
+        ]);
+        items.push(new DaysSinceItem("New Item", false));
     }
 
     return (
         <div className="items-center border w-screen h-screen">
-            <DaysSinceItem name="Test Item" isFinished={false} />
+            {
+                items.map(
+                    (item) =>
+                    { return <DaysSinceItem name={item.name} isFinished={item.isFinished} />; }
+                )}
             <button id="addItemButton" onClick={addItem} className="text-[#0a9ef0] bg-[#c0c0c0] px-3 py-1 rounded-md">Add Item</button>
         </div>
         
--- asp_website.client/src/components/DaysSinceItem.jsx
+++ asp_website.client/src/components/DaysSinceItem.jsx
@@ -5,7 +5,7 @@
 
 function DaysSinceItem({ name, isFinished }) {
     const itemColor = isFinished ? 'bg-rose-700' : 'bg-emerald-700';
-    const divClass = itemColor + " text-white w-4/5 h-1/6";
+    const divClass = itemColor + " text-white w-4/5 h-1/6 mt-2 mb-2";
 
     return (
         <div
