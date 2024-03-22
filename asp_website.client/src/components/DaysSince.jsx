d3229c02771eb5af75d786870dd00e7d8f9abc3e
Mike Damert <mtdamert@gmail.com>
2024-03-22 15:38:59 -0400

    Started adding a button to delete items

--- asp_website.client/src/components/DaysSince.jsx
+++ asp_website.client/src/components/DaysSince.jsx
@@ -3,13 +3,21 @@ import DaysSinceItem from "./DaysSinceItem";
 
 function DaysSince() {
     const [items, setItems] = useState([]);
+    const [itemCounter, setItemCounter] = useState(1);
 
-    const addItem = () => {
+    const addItem = (counter) => {
         setItems([
             ...items,
-            new DaysSinceItem("New Item", false)
+            new DaysSinceItem("New Item", false, counter)
         ]);
-        items.push(new DaysSinceItem("New Item", false));
+
+        console.log("Adding new item to queue: " + counter);
+        setItemCounter(counter + 1);
+    }
+
+    const deleteItem = (id) => {
+        console.log("Trying to delete item: " + id);
+        items.filter(a => a.id !== id);
     }
 
     return (
@@ -17,9 +25,9 @@ function DaysSince() {
             {
                 items.map(
                     (item) =>
-                    { return <DaysSinceItem name={item.name} isFinished={item.isFinished} />; }
+                    { return <DaysSinceItem name={item.name} isFinished={item.isFinished} id={item.itemCounter} deleteClick={deleteItem} />; }
                 )}
-            <button id="addItemButton" onClick={addItem} className="text-[#0a9ef0] bg-[#c0c0c0] px-3 py-1 rounded-md">Add Item</button>
+            <button id="addItemButton" onClick={() => addItem({ itemCounter })} className="text-[#0a9ef0] bg-[#c0c0c0] px-3 py-1 rounded-md">Add Item</button>
         </div>
         
     );
--- asp_website.client/src/components/DaysSinceItem.jsx
+++ asp_website.client/src/components/DaysSinceItem.jsx
@@ -3,14 +3,18 @@
 // TODO: Color (green/red)
 // TODO: Name
 
-function DaysSinceItem({ name, isFinished }) {
+function DaysSinceItem({ name, isFinished, id, deleteClick }) {
     const itemColor = isFinished ? 'bg-rose-700' : 'bg-emerald-700';
-    const divClass = itemColor + " text-white w-4/5 h-1/6 mt-2 mb-2";
+    const divClass = itemColor + " text-white w-4/5 h-1/6 mt-2 mb-2 text-xl";
 
     return (
         <div
-            className={ divClass }>
-            {name}
+            contentEditable={true}
+            className={divClass}
+            id={id}
+        >
+            <span>{name}foo</span>
+            <span onClick={() => deleteClick(id)} className="float-right">X</span>
         </div>
     );
 }
