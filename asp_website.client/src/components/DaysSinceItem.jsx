b657e75b6a96a5cdf8d7eb4fd7515f90f870ae83
Mike Damert <mtdamert@gmail.com>
2024-04-05 10:00:25 -0400

    Right justify the close button

--- asp_website.client/src/components/DaysSinceItem.jsx
+++ asp_website.client/src/components/DaysSinceItem.jsx
@@ -5,15 +5,15 @@
 
 function DaysSinceItem({ name, isFinished, id, deleteClick }) {
     const itemColor = isFinished ? 'bg-rose-700' : 'bg-emerald-700';
-    const divClass = itemColor + " text-white w-4/5 h-1/6 mt-2 mb-2 text-xl flex";
+    const divClass = itemColor + " flex justify-between text-white w-4/5 h-1/6 mt-2 mb-2 text-xl";
 
     return (
         <div
             className={divClass}
             id={id}
         >
-            <span className="flex items-center justify-center" contentEditable={true}>{name}</span>
-            <span onClick={() => deleteClick(id)} className="flex justify-end">X</span>
+            <div className="flex items-center justify-center" contentEditable={true}>{name}</div>
+            <div onClick={() => deleteClick(id)} className="flex justify-end">X</div>
         </div>
     );
 }
