dec5f812fddd81616f7f5519b2a1b6848f9a7157
Mike Damert <mtdamert@gmail.com>
2024-04-08 11:12:31 -0400

    Added a date to Days Since items

--- asp_website.client/src/components/DaysSinceItem.jsx
+++ asp_website.client/src/components/DaysSinceItem.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 // TODO: Last Date
 // TODO: Color (green/red)
@@ -7,12 +10,15 @@ function DaysSinceItem({ name, isFinished, id, deleteClick }) {
     const itemColor = isFinished ? 'bg-rose-700' : 'bg-emerald-700';
     const divClass = itemColor + " flex justify-between text-white w-4/5 h-1/6 mt-2 mb-2 text-xl";
 
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
         <div
             className={divClass}
             id={id}
         >
             <div className="flex items-center justify-center" contentEditable={true}>{name}</div>
+            <div className="flex items-end justify-end"><DatePicker showIcon selected={startDate} onChange={(date) => setStartDate(date)} /></div>
             <div onClick={() => deleteClick(id)} className="flex justify-end">X</div>
         </div>
     );
