e797ba71a831cd589816cf3d39f8bee7f89110b7
Mike Damert <mtdamert@gmail.com>
2024-04-11 15:55:17 -0400

    Updated About page

--- asp_website.client/src/components/About.jsx
+++ asp_website.client/src/components/About.jsx
@@ -1,6 +1,14 @@
 function About() {
     return (
-        <div className="flex items-center px-3 py-1.5 border">Hi, I'm Mike. </div>
+        <div className="flex items-center px-3 py-1.5 border">
+            <div>
+                Hi, I'm Mike.
+            </div>
+            <div>
+                This website was created as an attempt to improve my coding skills. The back end, used to save data, is written in C# and ASP.NET Core 3.1.
+                The front end is HTML/JavaScript/TypeScript/Vite/ReactJS.
+            </div>
+        </div>
     );
 }
 
