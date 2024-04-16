7fbb4a4cc4ab2a89835088faa069ad00ae2987cf
Mike Damert <mtdamert@gmail.com>
2024-04-16 14:48:09 -0400

    Added buttons to change Days Since item colors

--- asp_website.client/src/components/DaysSinceItem.jsx
+++ asp_website.client/src/components/DaysSinceItem.jsx
@@ -2,16 +2,13 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-// TODO: Last Date
-// TODO: Color (green/red)
-
 function DaysSinceItem({ name, isFinished, id, deleteClick }) {
     const itemColor = isFinished ? 'bg-rose-700' : 'bg-emerald-700';
-    const divClass = itemColor + " flex justify-between text-white w-4/5 h-1/6 mt-2 mb-2 text-xl";
 
     const [startDate, setStartDate] = useState(new Date());
     const [text, setText] = useState(name);
     const [dateText, setDateText] = useState("NOW");
+    const [itemStyle, setItemStyle] = useState(itemColor + " flex justify-between text-white w-4/5 h-1/6 mt-2 mb-2 text-xl");
 
     const getTimeDifference = (date) =>
     {
@@ -25,9 +22,12 @@ function DaysSinceItem({ name, isFinished, id, deleteClick }) {
         }
     }
 
+    const setColorRed = () => { setItemStyle("bg-rose-700 flex justify-between text-white w-4/5 h-1/6 mt-2 mb-2 text-xl"); }
+    const setColorGreen = () => { setItemStyle("bg-emerald-700 flex justify-between text-white w-4/5 h-1/6 mt-2 mb-2 text-xl"); }
+
     return (
         <div
-            className={divClass}
+            className={itemStyle}
             id={id}
         >
             <div className="flex items-center justify-center" contentEditable={true} onChange={(newText) => setText(newText)} suppressContentEditableWarning={true}>
@@ -35,6 +35,10 @@ function DaysSinceItem({ name, isFinished, id, deleteClick }) {
             </div>
             <div className="flex items-end justify-end text-black">
                 <DatePicker onChange={(date) => { setStartDate(date); setDateText(getTimeDifference(date)); }} placeholderText={dateText} />
+            </div>
+            <div className="flex">
+                <span id="red" className="border-2 h-10 w-10 bg-rose-700" onClick={setColorRed}></span>
+                <span id="blue" className="border-2 h-10 w-10 bg-emerald-700" onClick={setColorGreen}></span>
             </div>
             <div onClick={() => deleteClick(id)} className="flex justify-end">
                 X
