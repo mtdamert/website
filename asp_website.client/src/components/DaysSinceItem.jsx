5858b8a369adba00148590eb6ed7c4395e833254
Mike Damert <mtdamert@gmail.com>
2024-04-29 10:03:02 -0400

    Fixed blocks that let you select a color

--- asp_website.client/src/components/DaysSinceItem.jsx
+++ asp_website.client/src/components/DaysSinceItem.jsx
@@ -38,12 +38,13 @@ function DaysSinceItem({ name, isFinished, id, deleteClick }) {
                 {text}
             </div>
             <div className="basis-full h-0"></div>
-            <div className="items-end justify-end text-black grow col-start-2">
-                <DatePicker onChange={(date) => { setStartDate(date); setDateText(getTimeDifference(date)); }} placeholderText={dateText} />
+            <div className="items-end justify-end grow col-start-2">
+                <span>when: </span>
+                <span className="text-black font-bold"><DatePicker onChange={(date) => { setStartDate(date); setDateText(getTimeDifference(date)); }} placeholderText={dateText} /></span>
             </div>
-            <div className="w-1/2">
-                <span id="red" className="border-2 h-10 w-10 bg-rose-700" onClick={setColorRed}></span>
-                <span id="blue" className="border-2 h-10 w-10 bg-emerald-700" onClick={setColorGreen}></span>
+            <div className="w-24 h-10 inline-block">
+                <span id="red" className="border-2 h-10 w-10 bg-rose-700 inline-block" onClick={setColorRed}></span>
+                <span id="blue" className="border-2 h-10 w-10 bg-emerald-700 inline-block" onClick={setColorGreen}></span>
             </div>
         </div>
     );
