43ebba5d311cda23c98bee4c2c35ef8ce26d8c3c
Mike Damert <mtdamert@gmail.com>
2024-05-06 11:46:13 -0400

    Initial add of Days Since Event objects. Not hooked up yet.

---
+++ asp_website.Server/Controllers/DaysSinceEventController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace asp_website.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DaysSinceEventController : ControllerBase
+    {
+        DaysSinceEvent[]? daysSinceEvents;
+        const string daysSinceEventsPath = "DaysSinceEvents.txt";
+
+        public DaysSinceEventController()
+        {
+            // Load days since events from file 
+            string json = System.IO.File.ReadAllText(daysSinceEventsPath);
+            DaysSinceEvent[]? items = JsonSerializer.Deserialize<DaysSinceEvent[]>(json);
+            if (items != null && items.Length > 0)
+                daysSinceEvents = items;
+        }
+
+        [HttpGet(Name = "GetDaysSinceEvents")]
+        public DaysSinceEvent[]? Get()
+        {
+            return daysSinceEvents;
+        }
+
+        [HttpPost(Name = "SetDaysSinceEvents")]
+        public void Post([FromBody] DaysSinceEvent[] currentDaysSinceEvents)
+        {
+            // Save data to a file
+            daysSinceEvents = currentDaysSinceEvents;
+
+            string jsonString = JsonSerializer.Serialize(currentDaysSinceEvents);
+            using (StreamWriter writer = new StreamWriter(daysSinceEventsPath))
+            {
+                writer.WriteLine(jsonString);
+            }
+        }
+    }
+}
---
+++ asp_website.Server/DaysSinceEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace asp_website.Server
+{
+    public class DaysSinceEvent
+    {
+        public string? EventName { get; set; }
+
+        public DateTime? EventWhen { get; set; }
+
+        public string? ItemColor { get; set; }
+    }
+}
