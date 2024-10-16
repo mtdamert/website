8ba7da1052c6834c6f917baceaf22fa3ae09e3e9
Mike Damert <mtdamert@gmail.com>
2024-10-16 11:19:00 -0400

    Moved vertex shader to an external file

---
+++ asp_website.client/src/shaders/waveTestVertexShader.glsl
@@ -0,0 +1,21 @@
+﻿
+//const vertShader =
+// code from tutorial:
+// https://blog.maximeheckel.com/posts/the-study-of-shaders-with-react-three-fiber/
+
+uniform float u_time;
+
+varying vec2 vUv;
+
+void main() {
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  modelPosition.y += sin(modelPosition.x * 4.0 + u_time * 2.0) * 0.2;
+  
+  // Uncomment the code and hit the refresh button below for a more complex effect
+  // modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * 0.1;
+
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+}
