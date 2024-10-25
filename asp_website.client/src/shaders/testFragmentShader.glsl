bae4dd094c3b3d98696188fef8e667b6ada3dd0c
Mike Damert <mtdamert@gmail.com>
2024-10-25 14:22:02 -0400

    Added mouse hover effect; change colors over time; etc.

--- asp_website.client/src/shaders/testFragmentShader.glsl
+++ asp_website.client/src/shaders/testFragmentShader.glsl
@@ -1,14 +1,16 @@
 varying vec2 vUv;
+varying float vZ;
 
 uniform float u_time;
+uniform bool u_hover;
 vec3 colorA = vec3(0.912,0.191,0.652);
 vec3 colorB = vec3(1.000,0.777,0.052);
 
 void main() {
-  // "Normalizing" with an arbitrary value
-  // We'll see a cleaner technique later :)   
-  vec2 normalizedPixel = gl_FragCoord.xy/600.0;
-  vec3 color = mix(colorA, colorB, normalizedPixel.x);
+  float editedTime = u_hover ? 0.0 : u_time;
+
+  colorA.x = editedTime * vZ * 0.2 + 2.0;
+  vec3 color = mix(colorA, colorB, vZ * 2.0 + 0.5);
 
   gl_FragColor = vec4(color,1.0);
 }
