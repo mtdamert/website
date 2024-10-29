f9fe68653594491302f93c63d5bab2253dc82d40
Mike Damert <mtdamert@gmail.com>
2024-10-29 10:47:25 -0400

    Don't blow colors out

--- asp_website.client/src/shaders/testFragmentShader.glsl
+++ asp_website.client/src/shaders/testFragmentShader.glsl
@@ -7,7 +7,7 @@ vec3 colorA = vec3(0.912,0.191,0.652);
 vec3 colorB = vec3(1.000,0.777,0.052);
 
 void main() {
-  float editedTime = u_hover ? 0.0 : u_time;
+  float editedTime = u_hover ? 0.0 : min(u_time, 35.0);
 
   colorA.x = editedTime * vZ * 0.2 + 2.0;
   vec3 color = mix(colorA, colorB, vZ * 2.0 + 0.5);
