54a1f6599b72c93f6c462438ab59c5f5ca0d748c
Mike Damert <mtdamert@gmail.com>
2024-10-29 10:50:44 -0400

    Refactored for readability

--- asp_website.client/src/shaders/waveTestVertexShader.glsl
+++ asp_website.client/src/shaders/waveTestVertexShader.glsl
@@ -3,23 +3,21 @@
 // https://blog.maximeheckel.com/posts/the-study-of-shaders-with-react-three-fiber/
 
 uniform float u_time;
+uniform bool u_hover;
 
 varying vec2 vUv;
 varying float vZ;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.y += sin(modelPosition.x * 4.0 + u_time * 2.0) * 0.2;
-  
-  // Uncomment the code and hit the refresh button below for a more complex effect
-   modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * 0.1;
-
-   vZ = modelPosition.y;
+  modelPosition.y += sin(modelPosition.x * 5.0 + u_time * 3.0) * (u_hover ? 0.05 : 0.1);
+  modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * (u_hover ? 0.05 : 0.1);
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
-   vUv = uv;
+  vUv = uv;
+  vZ = modelPosition.y;
 
   gl_Position = projectedPosition;
 }
