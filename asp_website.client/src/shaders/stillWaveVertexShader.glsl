f9365a6cfb892ad740830ee52fae88237e5750f9
Mike Damert <mtdamert@gmail.com>
2024-11-19 09:20:24 -0500

    Increased scale of waves

--- asp_website.client/src/shaders/stillWaveVertexShader.glsl
+++ asp_website.client/src/shaders/stillWaveVertexShader.glsl
@@ -1,8 +1,8 @@
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.y += sin(modelPosition.x * 15.0) * 0.02;
-  modelPosition.y += sin(modelPosition.z * 12.0) * 0.02;
+  modelPosition.y += sin(modelPosition.x * 15.0) * 0.2;
+  modelPosition.y += sin(modelPosition.z * 12.0) * 0.2;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
