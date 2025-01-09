68309921311a42d858502ac6d7553cd90dd0af7d
Mike Damert <mtdamert@gmail.com>
2025-01-09 13:34:00 -0500

    Updated server to get username and password; moved token to external file

--- asp_website.client/server.cjs
+++ asp_website.client/server.cjs
@@ -1,12 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('node:fs');
+
+// Load a token from file
+let token = null;
+fs.readFile('token.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    token = data.trim();
+    console.log(token);
+});
 
 const app = express();
 
 app.use(cors());
-app.use('/login', (req, res) => {
+app.use(express.json());
+app.post('/login', (req, res) => {
+    console.log("called LOGIN; received " + req.method + ": " + req.body.username + ", " + req.body.password);
+
     res.send({
-        token: 'test123'
+        token: token
     });
 });
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
