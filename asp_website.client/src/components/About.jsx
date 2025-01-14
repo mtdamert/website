2c89eb2b802508410b21e640fd9957f38052069d
Mike Damert <mtdamert@gmail.com>
2025-01-14 10:42:51 -0500

    Clarified that I'm using ASP.NET

--- asp_website.client/src/components/About.jsx
+++ asp_website.client/src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
             </div>
             <div>
                 Welcome to my personal web site of tests & games.
-                The back end, used to save high scores in games, is written in <span className="font-bold">C#</span> and <span className="font-bold">.NET 8</span>.
+                The back end, used to save high scores in games, is written in <span className="font-bold">C#</span> and <span className="font-bold">ASP.NET 8</span>.
             </div>
             <div>
                 The front end is <span className="font-bold">HTML</span>/<span className="font-bold">JavaScript</span>/<span className="font-bold">TypeScript</span>/<span className="font-bold">Vite</span>/<span className="font-bold">ReactJS</span>.
