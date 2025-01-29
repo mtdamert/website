06e66fa5be8bbf3e3ac74fa3263a878b2abb64ce
Mike Damert <mtdamert@gmail.com>
2025-01-29 10:36:16 -0500

    Check whether adding a new user was successful or not

--- asp_website.client/src/components/SignUp.jsx
+++ asp_website.client/src/components/SignUp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Navigate, useNavigate } from 'react-router-dom';
 
 async function addNewUser(credentials) {
     console.log("sending to server: " + JSON.stringify(credentials));
@@ -11,22 +12,32 @@ async function addNewUser(credentials) {
         },
         body: JSON.stringify(credentials)
     });
+
+    return success;
 }
 
 function SignUp(credentials) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         // TODO: Add a new user
-        const token = await addNewUser({
+        const userAdded = await addNewUser({
             username,
             password
         });
 
         // TODO: Redirect to the previous page? Or to the logon page?
+        if (userAdded === true) {
+            navigate('/about');
+
+        } else {
+            console.log("Adding new user unsuccessful. Does this user already exist?");
+        }
     }
 
     return (
