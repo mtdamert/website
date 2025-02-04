e85c9d4e96fc866f75af95d83f87095dbcb9747c
Mike Damert <mtdamert@gmail.com>
2025-02-04 14:46:14 -0500

    Initial add; used for serializing and deserializing login data

---
+++ asp_website.Server/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿
+namespace asp_website.Server.Models
+{
+    [Serializable()]
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public byte[] SaltedHash { get; set; }
+        public byte[] Salt { get; set; }
+    }
+}
