4d3c12257594eaf6158830ed7deea03ed473038a
Mike Damert <mtdamert@gmail.com>
2025-02-11 14:19:32 -0500

    Added user roles of admin and client

--- asp_website.Server/Models/UserInfo.cs
+++ asp_website.Server/Models/UserInfo.cs
@@ -4,8 +4,12 @@ namespace asp_website.Server.Models
     [Serializable()]
     public class UserInfo
     {
+        public static readonly int Admin = 1;
+        public static readonly int Client = 2;
+
         public string Username { get; set; }
         public byte[] SaltedHash { get; set; }
         public byte[] Salt { get; set; }
+        public int UserRole { get; set; }
     }
 }
