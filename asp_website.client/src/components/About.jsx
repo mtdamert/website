e9a6972b235094dfdfe0c4f75da8fc449d72b56b
Mike Damert <mtdamert@gmail.com>
2025-04-09 10:08:19 -0400

    Updated CSS

--- asp_website.client/src/components/About.jsx
+++ asp_website.client/src/components/About.jsx
@@ -1,18 +1,17 @@
 function About() {
     return (
-        <div className="items-center px-3 py-1.5 border">
-            <div className="text-3xl">
+        <div className="items-center px-3 pb-1.5 border">
+            <div className="pl-12 text-4xl">
                 Michael Damert
             </div>
-            <div className="py-2">
-                Hi, I'm Mike. A full-stack developer.
+            <div className="pl-16 pb-12 text-xl">
+                full-stack developer
             </div>
-            <div className="py-2">
-                Welcome to my personal web site of tests & games.
-                The back end, used to save high scores in games, is written in <span className="font-bold">C#</span> and <span className="font-bold">ASP.NET MVC 8</span>.
+            <div className="italic">
+                The back end of this site is written in <span className="font-bold">C#</span> and <span className="font-bold">ASP.NET MVC 8</span>
             </div>
-            <div className="py-2">
-                The front end is <span className="font-bold">HTML 5</span>, <span className="font-bold">JavaScript</span>, <span className="font-bold">TypeScript</span>, <span className="font-bold">Vite</span>, and <span className="font-bold">ReactJS</span>.
+            <div className="italic">
+                The front end uses <span className="font-bold">HTML 5</span>, <span className="font-bold">JavaScript</span>, <span className="font-bold">TypeScript</span>, <span className="font-bold">Vite</span>, and <span className="font-bold">ReactJS</span>
             </div>
         </div>
     );
