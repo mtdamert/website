463ab63d32611657025546b406c10ce7ccf23e31
Mike Damert <mtdamert@gmail.com>
2025-04-23 14:04:23 -0400

    Added initial version of a retirement calculator controller to save data

---
+++ asp_website.Server/Controllers/RetirementCalcController.cs
@@ -0,0 +1,71 @@
+﻿using asp_website.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace asp_website.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RetirementCalcController : ControllerBase
+    {
+        List<RetirementCalcData>? allRetirementCalcData;
+        const string retirementCalcDataPath = "RetirementCalcData.txt";
+
+        // TODO: Handle in database
+        public RetirementCalcController()
+        {
+            if (System.IO.File.Exists(retirementCalcDataPath))
+            {
+                // Load all retirement calc data from file 
+                string json = System.IO.File.ReadAllText(retirementCalcDataPath);
+                if (!string.IsNullOrEmpty(json))
+                {
+                    List<RetirementCalcData>? items = JsonSerializer.Deserialize<List<RetirementCalcData>>(json);
+                    if (items != null && items.Count > 0)
+                        allRetirementCalcData = items;
+                }
+            }
+
+        }
+
+        private void Save()
+        {
+            // TODO: Write this
+        }
+
+        [HttpGet]
+        public RetirementCalcData? Get(string emailAddress)
+        {
+            if (emailAddress != null && allRetirementCalcData != null)
+            {
+                RetirementCalcData? data = allRetirementCalcData.FirstOrDefault(data => data.emailAddress == emailAddress);
+                return data;
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] RetirementCalcData updatedData)
+        {
+            if (allRetirementCalcData == null)
+            {
+                allRetirementCalcData = new List<RetirementCalcData>();
+            }
+
+            RetirementCalcData? data = allRetirementCalcData.FirstOrDefault(data => data.emailAddress == updatedData.emailAddress);
+            if (data != null)
+            {
+                // Update this data. TODO: Test this line
+                data = updatedData;
+            }
+            else
+            {
+                allRetirementCalcData.Add(updatedData);
+            }
+
+            Save();
+        }
+
+    }
+}
---
+++ asp_website.Server/Models/RetirementCalcData.cs
@@ -0,0 +1,22 @@
+﻿
+namespace asp_website.Server.Models
+{
+    [Serializable()]
+    public class RetirementCalcData
+    {
+        public string? emailAddress {  get; set; }
+        public int? age {  get; set; }
+        public int? currentSavings { get; set; }
+        public int? currentPostTaxSavings { get; set; }
+        public int? currentRetirementSavings { get; set; }
+        public int? currentIncome { get; set; }
+        public int? livingExpenses { get; set; }
+        public int? retirementAge { get; set; }
+        public int? startSocialSecurityAge { get; set; }
+        public int? estSocialSecurityIncome { get; set; }
+        public int? predictedYieldPct { get; set; }
+        public int? cpiPct { get; set; }
+        public int? ageAtDeath {  get; set; }
+        public bool? extrapolateCapitalGains { get; set; }
+    }
+}
