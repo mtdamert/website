3cf834188f9713ae615a7e1075bce6deb3216da2
Mike Damert <mtdamert@gmail.com>
2025-06-24 15:24:15 -0400

    Added a title to the Update My Account page

--- asp_website.client/src/components/MyAccount.jsx
+++ asp_website.client/src/components/MyAccount.jsx
@@ -31,16 +31,17 @@ function MyAccount({ emailAddress, username, setToken }) {
 
     return (
         <div class="content">
+            <div class="title">Update Your User Info</div>
             <form onSubmit={handleSubmit} className="p-2 bg-blue-200">
                 <div>
-                    <span className="font-bold">Username: </span>
+                    <span className="font-bold">New Username: </span>
                     <input id="userInput" value={userInput} onChange={e => { setUserInput(e.target.value); setIsSaveDisabled(false); } } className="border-2" />
                 </div>
                 <div className="pt-2">
-                    <span className="font-bold">Password: </span>
+                    <span className="font-bold">New Password: </span>
                     <input onChange={e => { setPassInput(e.target.value); setIsSaveDisabled(false); } } className="border-2" />
                 </div>
-                <div>etc.</div>
+                {/*<div>etc.</div>*/}
                 <div className="pt-8 place-self-center">
                     <button type="submit" disabled={isSaveDisabled}>Update</button>
                 </div>
